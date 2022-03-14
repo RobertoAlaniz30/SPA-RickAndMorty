05d23c5ceafced5daa9a03a6b5f4284a317b6723
RobertoAlaniz30 <roberto.alaniz3002@gmail.com>
2022-03-14 12:19:34 -0600

    Actualizacion de repositorio

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+const About = () => {
+    const view = `
+    <p> </p>
+      <div class="about">
+        <p>Welcome to my rick and morty page, 
+          a small project where you can obtain information of the characters of this animated series, 
+          through a request to the rick and morty api, 
+          enter the following url to see the complete repository.
+        </p>
+        <a href="/jsi/asadv">
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" />
+        </a>
+      </div>
+      `;
+    return view;
+  };
+  
+  export default About;
---
+++ src/template/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    const view = `
+      <div class="Header-main">
+        <div class="Header-logo">
+            <a class="link-logo" href="/">
+              <img src="http://assets.stickpng.com/images/58f37720a4fa116215a9240f.png" />
+            </a>
+        </div>
+        <div class="Header-nav">
+          <a href="#/about/">
+            About
+          </a>
+          <a href="#/">
+            Home
+          </a>
+        </div>
+      </div>
+    `;
+    return view;
+  };
+  
+  export default Header;
